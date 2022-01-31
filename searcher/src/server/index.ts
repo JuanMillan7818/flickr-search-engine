98bebdd5d25b805e8b3d423f75870334908aedc6
Juan Pablo <juan.holgin@correounivalle.edu.co>
2022-01-31 16:53:38 -0500

    Cambio de puerto

--- searcher/src/server/index.ts
+++ searcher/src/server/index.ts
@@ -3,7 +3,7 @@ import * as dotenv from 'dotenv';
 
 const start = () => {
   dotenv.config();
-  let port = parseInt(process.env.PORT_SERVE || '');
+  let port = parseInt(process.env.PORT_SERVE || '3000');
   let app = new App(port);  
   
   app.listen();
