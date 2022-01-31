5284622468d63434346747ed4a5ff4c5ce4bc314
Juan Pablo <juan.holgin@correounivalle.edu.co>
2022-01-31 17:04:06 -0500

    Configuracion para produccion

--- searcher/ormconfig.ts
+++ searcher/ormconfig.ts
@@ -1,13 +1,29 @@
 import { RecordSearch } from "./src/server/db/entities/record-search.entity";
 
-export default {
-    "type": "postgres",
-    "host": process.env.POSTGRES_HOST,
-    "port": process.env.POSTGRES_PORT,
-    "username": process.env.POSTGRES_USER,
-    "password": process.env.POSTGRES_PASSWORD,
-    "database": process.env.POSTGRES_DB,
-    "entities": [RecordSearch],
-    "synchronize": true,           
-    "autoLoadEntities": true,      
+let config: object = {};
+if(process.env.NODE_ENV === 'production') {
+    config = {
+        url: process.env.DATABASE_URL,
+        type: 'postgres',
+        ssl: {
+            rejectUnauthorized: false,
+        },          
+        entities: [RecordSearch],
+        synchronize: true,           
+        autoLoadEntities: true
+    }
+}else {
+    config = {
+        "type": "postgres",
+        "host": process.env.POSTGRES_HOST,
+        "port": process.env.POSTGRES_PORT,
+        "username": process.env.POSTGRES_USER,
+        "password": process.env.POSTGRES_PASSWORD,
+        "database": process.env.POSTGRES_DB,
+        "entities": [RecordSearch],
+        "synchronize": true,           
+        "autoLoadEntities": true,      
+    }
 }
+
+export default config;
